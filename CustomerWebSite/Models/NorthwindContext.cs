2ac426931c00c9efbb7f97aeb44ca55e9b01b445
allen <zozo6616833@gmail.com>
2024-02-02 14:00:38 +0800

    ajax最終版本 +上跨網頁抓取資料

--- CustomerWebSite/Models/NorthwindContext.cs
+++ CustomerWebSite/Models/NorthwindContext.cs
@@ -17,9 +17,9 @@ public partial class NorthwindContext : DbContext
 
     public virtual DbSet<Customer> Customers { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Data Source=.;TrustServerCertificate=true;Initial CataLog=Northwind;Integrated Security=true");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.;TrustServerCertificate=true;Initial CataLog=Northwind;Integrated Security=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
